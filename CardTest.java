58a0b8e9fd96dba80aba904b8fb5d3b80ea3f932
Abdullah Khan <ak7256369@gmail.com>
2023-09-25 16:42:16 +0500

    Card Game

---
+++ CardTest.java
@@ -0,0 +1,12 @@
+package week4;
+
+public class CardTest {
+    public static void main(String[] args) {
+        Deck d1 = new Deck();
+        d1.shuffle();
+        System.out.println(d1);
+    }
+}
+
+
+
